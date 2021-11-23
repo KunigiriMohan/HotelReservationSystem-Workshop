aeb11767202877bd23a21056640669eb50e49b00
KunigiMohan <kmohanr5@gmail.com>
2021-11-23 12:00:09 +0530

    Added : modified add method for taking weekdays and weekend data.

--- src/HotelReservation.java
+++ src/HotelReservation.java
@@ -3,13 +3,13 @@ import java.util.Map;
 
 public class HotelReservation {
     public static Map<String,Hotel> hotels= new LinkedHashMap<>();                  //Creating map for storing hotel details
-
-    public int add(String hotel_Name, int rate_Regular_Weekdays, int rate_Weekends)
+/*
+* add() method to add object elements to map
+* */
+    public int add(String hotel_Name, int rate_Regular_Weekdays, int rate_Weekends, int reward_Customer_Weekday_rates, int reward_Customer_Weekend_rates)
     {
-        System.out.println("Welcome to Hotel Reservation System.");
-        Hotel obj= new Hotel(hotel_Name,rate_Regular_Weekdays,rate_Weekends);           //Creating objects of hotel class by sending values
+        Hotel obj= new Hotel(hotel_Name,rate_Regular_Weekdays,rate_Weekends,reward_Customer_Weekday_rates,reward_Customer_Weekend_rates);
         hotels.put(hotel_Name,obj);
-
-        return hotels.size();
+        return hotels.size();           //returning size of hashmap
     }
 }
