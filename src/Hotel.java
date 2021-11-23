5ac6c0ce9c571a5ed2a0d86031d759ca04c15a60
KunigiMohan <kmohanr5@gmail.com>
2021-11-23 18:44:00 +0530

    Added : Refactoring code to add rating Parameter.

--- src/Hotel.java
+++ src/Hotel.java
@@ -3,15 +3,16 @@ public class Hotel {
     private String name;                //Instance Variables
     private int rate_Regular_Weekdays;
     private int weekEnd_Fare;
+    private int rating;
 
     /*
      * Constructor for initializing instance variables
      * */
-    public Hotel (String name,int rate_Regular_Weekdays,int weekEnd_Fare)
+    public Hotel (String name,int rate_Regular_Weekdays,int weekEnd_Fare,int rating)
     {
         this.name =name;
         this.rate_Regular_Weekdays=rate_Regular_Weekdays;
         this.weekEnd_Fare=weekEnd_Fare;
-
+        this.rating=rating;
     }
 }
