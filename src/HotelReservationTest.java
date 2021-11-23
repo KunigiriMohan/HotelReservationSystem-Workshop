bbd7ea7f74b30e6523f98ab2348856eb2feb1462
KunigiMohan <kmohanr5@gmail.com>
2021-11-23 12:01:27 +0530

    Added : Test case for testing add() method.

--- src/HotelReservationTest.java
+++ src/HotelReservationTest.java
@@ -1,10 +1,17 @@
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HotelReservationTest {
 
     @org.junit.jupiter.api.Test
-    void add() {
+    void add() throws ParseException {
         HotelReservation obj = new HotelReservation();
-        assertEquals(1,obj.add("LakeWood",110,90));
+
+        assertEquals(1,obj.add("LakeWood",110,90, 80, 80));
+        assertEquals(2,obj.add("BridgeWood",150,50, 60, 50));
+        assertEquals(3,obj.add("RidgeWood",220,150, 150, 40));
+
     }
+
 }
