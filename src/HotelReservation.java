e6d44cf0a87c6f08944880505c369d0029fa9ecb
KunigiMohan <kmohanr5@gmail.com>
2021-11-23 13:31:00 +0530

    Added :modified for taking inputs and returning hotel with minimum fare.

--- src/HotelReservation.java
+++ src/HotelReservation.java
@@ -1,15 +1,37 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+/*
+ * importing libraries
+ * */
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class HotelReservation {
-    public static Map<String,Hotel> hotels= new LinkedHashMap<>();                  //Creating map for storing hotel details
-/*
-* add() method to add object elements to map
-* */
-    public int add(String hotel_Name, int rate_Regular_Weekdays, int rate_Weekends, int reward_Customer_Weekday_rates, int reward_Customer_Weekend_rates)
+    public static Map<String,Hotel> hotels= new LinkedHashMap<>();              //Creating maps of hotel to store hotel name and hotel object
+    public static Map<Integer,String>regularRates=new LinkedHashMap<>();           //Creating regularRates to store hotel rates along with hotel name
+    public static Map<Integer,String >list=new LinkedHashMap<>();           //Creating list map to store rates of different hotels and its names.
+
+
+    public int add(String hotel_name,int rate_Regular_Weekdays)                 //add() method to add hotel details to various maps
     {
-        Hotel obj= new Hotel(hotel_Name,rate_Regular_Weekdays,rate_Weekends,reward_Customer_Weekday_rates,reward_Customer_Weekend_rates);
-        hotels.put(hotel_Name,obj);
-        return hotels.size();           //returning size of hashmap
+        hotels.put(hotel_name,new Hotel(hotel_name,rate_Regular_Weekdays));
+        regularRates.put(rate_Regular_Weekdays,hotel_name);
+        return hotels.size();
+
+    }
+    public Object rate_of_Hotels(String start_Date, String end_Date)
+    {                                                                                      //verifies date according to day and return minimum fare
+        LocalDate currentDate1 = LocalDate.parse(start_Date);
+        LocalDate currentDate2 = LocalDate.parse(end_Date);
+
+        if (currentDate1.getDayOfWeek() != DayOfWeek.SATURDAY && currentDate1.getDayOfWeek() != DayOfWeek.SUNDAY)
+        {
+            list = regularRates.entrySet().stream().collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+        }
+        if (currentDate2.getDayOfWeek() != DayOfWeek.SATURDAY && currentDate2.getDayOfWeek() != DayOfWeek.SUNDAY)
+        {
+            list = regularRates.entrySet().stream().collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+        }
+        return Collections.min(list.keySet());
     }
 }
